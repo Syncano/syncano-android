3e33551e0ccfd2e36e38730048d86fa66e43836b
Dawid Członka <dawid.czlonka@pixzell.pl>
2015-02-17 17:18:28 +0100

    Project clean.

--- SyncanoLib/instrumentTest/java/com/syncano/android/test/config/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.syncano.android.test.config;
-
-public class Constants {
-
-    // Test environment configuration
-    public static final String API_KEY = ""; // Admin or Owner Api Key
-    public static final String INSTANCE_NAME = "";
-
-}
