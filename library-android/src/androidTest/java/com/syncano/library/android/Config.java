9380fd438f6710599b79fd095bab4d09d8847e80
Dawid Członka <dawid.czlonka@pixzell.pl>
2015-03-24 09:12:54 +0100

    Remove test credentials.

--- library-android/src/androidTest/java/com/syncano/library/android/Config.java
+++ library-android/src/androidTest/java/com/syncano/library/android/Config.java
@@ -1,6 +1,6 @@
 package com.syncano.library.android;
 
 public class Config {
-    public static final String API_KEY = "c39742252034618f71c5d7e9ff556fe21464d0ee";
-    public static final String INSTANCE_NAME = "test_seba";
+    public static final String API_KEY = "";
+    public static final String INSTANCE_NAME = "";
 }
