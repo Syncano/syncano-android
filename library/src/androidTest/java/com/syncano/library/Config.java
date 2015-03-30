ec5cc4997690da812f9085beefc487fc739df6d3
Dawid Członka <dawid.czlonka@pixzell.pl>
2015-03-30 13:22:24 +0200

    Added environment variables with sed.

--- library/src/androidTest/java/com/syncano/library/Config.java
+++ library/src/androidTest/java/com/syncano/library/Config.java
@@ -1,6 +1,6 @@
 package com.syncano.library;
 
 public class Config {
-    public static final String API_KEY = "";
-    public static final String INSTANCE_NAME = "";
+    public static final String API_KEY = "YOUR_API_KEY";
+    public static final String INSTANCE_NAME = "YOUR_INSTANCE_NAME";
 }
