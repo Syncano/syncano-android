9b2146c6e4d1cce15687e89d63e95dcc2d153b20
Pawel Szymanski <pawel.szymanski@chimeraprime.com>
2015-11-06 16:48:04 +0100

    Updated circle ci config and test server details

--- library/src/androidTest/java/com/syncano/library/Config.java
+++ library/src/androidTest/java/com/syncano/library/Config.java
@@ -1,7 +1,7 @@
 package com.syncano.library;
 
 public class Config {
-    public static final String STAGING_SERVER_URL = "https://syncanotest1-env.elasticbeanstalk.com";
     public static final String API_KEY = "YOUR_API_KEY";
     public static final String INSTANCE_NAME = "YOUR_INSTANCE_NAME";
+    public static final String STAGING_SERVER_URL = "https://api.syncano.io";
 }
